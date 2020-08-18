c88fa31973350a9847ddde8464cd45127d6b8d9f
hiroshi-morimoto <60027767+hiroshi-morimoto@users.noreply.github.com>
2020-08-18 20:09:57 +0900

    css

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'hp_top#index' # ログインor新規登録を選ぶページ
   resources :hp_top, only: :new  # 新規登録方法を選ぶページ
+  resources :about, only: :index
+  resources :projects, only: :index
+  resources :contact, only: :index
+  resources :donation, only: :index
 end
